dfd2f15d4c5855f9fd8243db24726276e9d34a0a
Douglas Abrams <Dougie@lawn-128-61-43-142.lawn.gatech.edu>
2013-03-29 13:48:29 -0400

    Database work - will break

--- DealGenda/migrations/002.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users_2 (id INTEGER PRIMARY KEY AUTOINCREMENT, email varchar(255), password varchar(255), fname varchar(255), lname varchar(255), gender varchar(255), birthday DATE)
-INSERT INTO users_2(email, password, fname, lname, gender, birthday) SELECT * FROM users
-ALTER TABLE users RENAME TO users_old
-ALTER TABLE users_2 RENAME TO users
