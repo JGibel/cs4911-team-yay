17525edb56fff265e42646855859851095cfdecd
Jenelle Walker <jen@gatech>
2013-04-28 18:02:27 -0400

    comments on AppDelegate.m

--- DealGenda/AppDelegate.m
+++ DealGenda/AppDelegate.m
@@ -13,6 +13,10 @@
 @synthesize db;
 @synthesize user;
 
+/**
+ * Default iOS method
+ * Calls database creationg and migration methods. This happens every time the app launches
+ **/
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [self createDatabase];
@@ -21,13 +25,20 @@
     // Override point for customization after application launch.
     return YES;
 }
-							
+
+/**
+ * Default iOS method
+ **/
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
+/**
+ * Default iOS method
+ * Makes sure database is closed when the application loses focus
+ **/
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
@@ -35,36 +46,51 @@
     [db close];
 }
 
+/**
+ * Default iOS method
+ **/
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    if (![db open]) {
-        return;
-    }
 }
 
+/**
+ * Default iOS method
+ **/
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
+/**
+ * Default iOS method
+ * Makes sure the database is closed when the application closes
+ **/
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     [db close];
 }
 
+/**
+ * if database does not exist, create a new directory Database in the Library directory and create a database DealGenda.db
+ * return:void
+ **/
 -(void)createDatabase
-//if database does not exist, create a new directory Database in the Library directory and create a database DealGenda.db
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    //path in app data Library directory
     NSString *databaseDirectory = [[paths objectAtIndex:0]stringByAppendingPathComponent:@"Database/"];
+    //extends path from Library directory to Library/Database
     NSString *dbPath = [databaseDirectory stringByAppendingPathComponent:@"DealGenda.db"];
+    //full path of database: Library/Database/DealGenda.db
     NSError *error;
     
     if (![[NSFileManager defaultManager] fileExistsAtPath:dbPath])
+        //checks if database exists
 	{
         if (![[NSFileManager defaultManager] createDirectoryAtPath:databaseDirectory
+              //create directory if files doesn't exist
 									   withIntermediateDirectories:NO
 														attributes:nil
 															 error:&error])
@@ -74,6 +100,7 @@
 	}
     
     db = [FMDatabase databaseWithPath:dbPath];
+    //set global directory variable to database in user's app data Library/Database/DealGenda.db
 }
 
 
